488376215290c5746e90b05d15b515200c272f0c
skiziltas <samet.kiziltas@hangikredi.com>
2022-09-18 14:45:16 +0300

    EventBus Base changes

---
+++ src/EventBus/EventBus.Base/Interfaces/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using EventBus.Base.Events;
+
+namespace EventBus.Base.Interfaces 
+{
+    public interface IIntegrationEventHandler<T>: IntegrationEventHandler where T : IntegrationEvent
+    {
+        Task Handle(T @event);
+    }
+
+    public interface IntegrationEventHandler 
+    {
+        
+    }
+}
